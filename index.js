49ca911a7a6f0b16baca6cae5bcfc49709c60575
Maukr <61480380+Maukr@users.noreply.github.com>
2020-03-07 13:16:59 -0300

    añado index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+let arrayColores = ['blue', 'blue', 'red', 'red', 'yellow', 'yellow', 'green', 'green', 'purple', 'purple', 'orange', 'orange'];
+
+function prepararJuego(){
+    let $cuadros = document.querySelectorAll(".cuadro");
+    setearColores($cuadros);
+    bloquearInputUsuario($cuadros);
+    mostrarColores($cuadros);
+
+}
+
+function setearColores($cuadros){
+    while(arrayColores.length > 0){
+        let pos = posicionAleatoria(arrayColores.length);
+        let color = arrayColores[pos];
+        arrayColores.splice(pos, 1);
+        $cuadros[arrayColores.length].style.backgroundColor = color;
+    }
+}
+
+function posicionAleatoria(max){
+    return Math.floor((Math.random() * max));
+}
+
+function mostrarColores($cuadros){
+
+    setTimeout(function(){
+        $cuadros.forEach(element => {
+            element.style.opacity = "1";
+        }); 
+    }, 500);
+
+    setTimeout(function(){
+        $cuadros.forEach(element => {
+            element.style.opacity = "0";
+        }); 
+    }, 2000);
+}
+
+function bloquearInputUsuario($cuadros){
+    $cuadros.forEach(element => {
+        element.onclick = function(){};
+    });
+}
+
+prepararJuego();
+
+
